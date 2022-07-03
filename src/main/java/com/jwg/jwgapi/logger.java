1f764681b7475766a8775fc5cb951b19c07404fe
JWGardiner <jamesgardiner340@gmail.com>
2022-07-03 15:25:03 +0100

    Fixed Logger

--- src/main/java/com/jwg/jwgapi/logger.java
+++ src/main/java/com/jwg/jwgapi/logger.java
@@ -4,10 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.DirectoryNotEmptyException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 import java.util.Scanner;
 
 public class logger {
@@ -34,21 +32,22 @@ public class logger {
         if (severity >= 4) {
             severity = 4;
         }
-        String logString = logSeverity[severity] + " (" + name + ", " + version + ") " + logMessage;
-        Scanner readLog = new Scanner(logFile);
         String log = "";
-        while (readLog.hasNextLine()) {
-            log = log + readLog.nextLine() + "\n";
-        }
-        readLog.close();
+        String logString = logSeverity[severity] + " (" + name + ", " + version + ") " + logMessage;
+
         try {
-            FileWriter myWriter = new FileWriter(logFile);
-            myWriter.write(log);
-            myWriter.close();
+            log = new String(Files.readAllBytes(Paths.get(logFile)), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        catch (IOException e) {
+        try {
+            FileWriter logWriter = new FileWriter(logFile);
+            logWriter.write(log + "\n" + logString);
+            logWriter.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(log);
         System.out.println(logString);
     }
 }
