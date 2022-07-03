978a7601129c0e66e1ec51a6cf5649b73aab8f3c
JWGardiner <jamesgardiner340@gmail.com>
2022-07-03 16:30:50 +0100

    Added Debug shorthands

---
+++ src/main/java/com/jwg/jwgapi/shorthands.java
@@ -0,0 +1,11 @@
+package com.jwg.jwgapi;
+
+
+public class shorthands {
+    public void debugLn(String text) {
+        System.out.println(text);
+    }
+    public void debug(String text) {
+        System.out.print(text);
+    }
+}
